e0fd0dafd0aa78f18ab6f218a6fe9c4ad0f93ecb
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2017-08-25 21:43:37 +0200

    Add Post & User models

---
+++ Demo/Models/Post.swift
@@ -0,0 +1,15 @@
+//
+//  Post.swift
+//  Demo
+//
+//  Created by Lukasz Mroz on 25.08.2017.
+//  Copyright © 2017 Łukasz Mróz. All rights reserved.
+//
+
+struct Post {
+
+    let id: Int
+    let userId: Int
+    let title: String
+    let body: String
+}
---
+++ Demo/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Demo
+//
+//  Created by Lukasz Mroz on 25.08.2017.
+//  Copyright © 2017 Łukasz Mróz. All rights reserved.
+//
+
+struct User {
+
+    let id: Int
+    let name: String
+    let username: String
+    let email: String
+}
