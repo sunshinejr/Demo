a4097e534432d47bd7c7a5f2823650bf85ad1dac
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2017-08-25 22:11:14 +0200

    Add really basic view model for post cell

---
+++ Demo/ViewControllers/Posts/ViewModels/PostTableViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PostCellViewModel.swift
+//  Demo
+//
+//  Created by Lukasz Mroz on 25.08.2017.
+//  Copyright © 2017 Łukasz Mróz. All rights reserved.
+//
+
+import RxCocoa
+
+struct PostTableViewCellViewModel {
+    let title: Driver<String>
+}
--- Demo/ViewControllers/Posts/Views/PostTableViewCell/PostTableViewCell.swift
+++ Demo/ViewControllers/Posts/Views/PostTableViewCell/PostTableViewCell.swift
@@ -7,6 +7,31 @@
 //
 
 import UIKit
+import RxSwift
 
 final class PostTableViewCell: UITableViewCell, ReusableCell {
+
+    typealias ViewModel = PostTableViewCellViewModel
+
+    @IBOutlet private weak var titleLabel: UILabel!
+
+    var viewModel: ViewModel? {
+        didSet {
+            clearCurrentViewModelSetup()
+
+            if let viewModel = viewModel {
+                setupViewModel(viewModel)
+            }
+        }
+    }
+
+    private var disposeBag = DisposeBag()
+
+    private func clearCurrentViewModelSetup() {
+        disposeBag = DisposeBag()
+    }
+
+    private func setupViewModel(_ viewModel: ViewModel) {
+        viewModel.title.drive(titleLabel.rx.text).addDisposableTo(disposeBag)
+    }
 }
